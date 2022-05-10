d52d2de56b2d34a35b25422ac5a5e130134b89aa
Derek J. Hunt <derek.hunt@gmail.com>
2022-05-10 15:39:33 -0400

    adjusted for case sensative issue for wikiquote api.

--- cogs/quote.py
+++ cogs/quote.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import random
 import wikiquote
+import string
 
 class Quote(commands.Cog): 
     def __init__(self, bot): # this is a special method that is called when the cog is loaded
@@ -12,12 +13,12 @@ class Quote(commands.Cog):
     async def quote(self, ctx, arg):
         """Pulls a random quote from WikiQuote. Example: !quote "George Washington" """
     #    try:
-        response = random.choice(wikiquote.quotes(arg))
+        response = random.choice(wikiquote.quotes(string.capwords(arg)))
         #except wikiquote.DisambiguationPageException as e:
         #  s = random.choice(e.options)
         #  response = wikiquote.quotes(s)
         arg = arg.replace('"', '')
-        response_a = arg + ": " + response
+        response_a = string.capwords(arg) + ": " + response
         await ctx.send(response_a)
 
 def setup(bot): # this is called by Pycord to setup the cog
